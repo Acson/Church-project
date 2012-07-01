a49f4f4d13c224853585fb5af21f2b48d6c45ee4
Acson <acsonbarreto@gmail.com>
2012-07-01 13:55:47 -0300

    Retirando os temas inuteis do github

--- wp-content/themes/headlines/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div id="sidebar" class="col-<?php if ( get_option('woo_left_sidebar') == "true" ) echo 'left'; else echo 'right'; ?>">
-
-	<!-- Widgetized Sidebar -->	
-	<?php woo_sidebar('sidebar-1'); ?>		
-             
-</div><!-- /#sidebar -->
